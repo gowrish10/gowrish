42e0a87a3c1464888ea733418b8e2f0d610194d7
gowrish <gojogowrish@gmail.con>
2025-06-09 16:09:52 +0530

    form added

---
+++ css/javascript/python/condition.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+if a > b:
+    print("a is Greater")
+    elif b>a and b>c:
+        print("b is Greater")
+    else:
+        print("c is Greater")
---
+++ css/javascript/script.js
@@ -0,0 +1,16 @@
+ 
+ <script>
+ console.log("hello world")
+        var a;//declaraction
+        a=10;//initializtion
+        var b=20;//declaraction with initializtion
+        //aruthmetic operators
+        console.log(a,b)
+        console.log(a+b)
+        console.log(a-b)
+        console.log(a*b)
+        console.log(a/b)
+        console.log(a%b)
+        </script>
+        </body>
+        </html>
